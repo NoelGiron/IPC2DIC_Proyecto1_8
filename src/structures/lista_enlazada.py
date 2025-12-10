0087ebedd345dc9f13547b02266ff489fc9608ef
NoelGiron <mngiron@hotmail.com>
2025-12-10 12:23:46 -0600

    se creo la estructura de una lista enlazada

---
+++ src/structures/lista_enlazada.py
@@ -0,0 +1,48 @@
+class nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
+
+class lista_enlazada:
+    def __init__(self):
+        self.primero = None
+        self.size = 0
+
+    def lista_vacia(self):
+        return self.primero == None
+    
+    def insertar(self, dato):
+        nuevo = nodo(dato)
+
+        if self.primero == None:
+            self.primero = nuevo
+        
+        else:
+            actual = self.primero
+            while actual.siguiente != None:
+                actual = actual.siguiente
+            actual.siguiente = nuevo
+        self.size += 1
+
+        def recorrer(self, indice):
+            actual = self.primero
+            contador = 0
+            
+            while actual != None:
+                if contador == indice:
+                    return actual.dato
+                actual = actual.siguiente
+                contador += 1
+            return None
+
+        def imprimir(self):
+            if self.primero == None:
+                print("La lista esta vacia")
+                return
+            
+            actual = self.primero
+            while actual.siguiente != None:
+                print(actual.dato)
+                actual = actual.siguiente
+            print(actual.dato)
+            
