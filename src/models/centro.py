3be01b5d5c9eebce3207cd071e574ff727162ae1
NoelGiron <mngiron@hotmail.com>
2025-12-10 17:57:56 -0600

    se creo la clase maquina_virtual

---
+++ src/models/centro.py
@@ -0,0 +1,9 @@
+class centro:
+    def __init__(self, id_cd, nombre, pais, ciudad, cpu, ram, almacenamiento):
+        self.id_cd = id_cd
+        self.nombre = nombre
+        self.pais = pais
+        self.ciudad = ciudad
+        self.cpu = cpu
+        self.ram = ram
+        self.almacenamiento = almacenamiento
---
+++ src/models/maquina_virtual.py
@@ -0,0 +1,9 @@
+class maquina_virtual:
+    def __init__(self, id_vm, centro_asig, os, cpu_vm, ram_vm, almacenamiento, ip_mv):
+        self.id_vm = id_vm
+        self.centro_asig = centro_asig
+        self.os = os
+        self.cpu_vm = cpu_vm
+        self.ram_vm = ram_vm
+        self.almacenamiento = almacenamiento
+        self.ip_vm = ip_mv
