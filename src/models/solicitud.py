3a9eee847867cf712d5c9bca8173b959ff773824
NoelGiron <mngiron@hotmail.com>
2025-12-10 18:28:35 -0600

    se creo la clase solicitud

---
+++ src/models/solicitud.py
@@ -0,0 +1,9 @@
+class solicitud:
+    def __init__(self, id_solic, cliente, tipo_solic, cpu_solic, ram_solic, almacenamiento_solic, tiempo_solic):
+        self.id_solic = id_solic
+        self.cliente = cliente
+        self.tipo_solic = tipo_solic
+        self.cpu_solic = cpu_solic
+        self.ram_solic = ram_solic
+        self.almacenamiento_solic = almacenamiento_solic
+        self.tiempo_solic = tiempo_solic 
