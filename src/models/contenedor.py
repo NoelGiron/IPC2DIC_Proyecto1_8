5f9741a6b277864a338d09d64411850cb1a1f3b0
NoelGiron <mngiron@hotmail.com>
2025-12-10 18:17:34 -0600

    se creo la clase contenedor

---
+++ src/models/contenedor.py
@@ -0,0 +1,9 @@
+class contenedor:
+    def __init__(self, id_cont, nombre_cont, img_cont, cpu_cont, ram_cont, almacenamiento_cont, puerto):
+        self.id_cont = id_cont
+        self.nombre_cont = nombre_cont
+        self.img_cont = img_cont
+        self.cpu_cont = cpu_cont
+        self.ram_cont = ram_cont
+        self.almacenamiento_cont = almacenamiento_cont
+        self.puerto = puerto
